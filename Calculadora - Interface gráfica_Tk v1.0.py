928db1b0105220e017b6ba352277c86212837308
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-04-04 15:30:07 -0300

    Add files via upload

---
+++ Calculadora - Interface gráfica_Tk v1.0.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import *
+
+janela = Tk()
+
+label_1 = Label(janela, text='Seu texto vai aqui', font = 'Arial 20')
+label_1.place(x=200,y=200)
+
+janela.geometry('800x600+0+0')
+janela.mainloop()
